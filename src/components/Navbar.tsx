e7cb14a4ad3ffd9b69d2fb58cda5d63745c75c46
nicol4ss <nicolas.elias@pm.me>
2023-02-22 23:40:23 -0300

    Iniciando projeto e configurando bold

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,50 @@
+import { Button, HFlow, Icon, VFlow } from "bold-ui";
+import { css } from "@emotion/core";
+import Logo from "../assets/crosshair.svg";
+
+function Navbar() {
+  return (
+    <VFlow
+      style={css`
+        align-items: center;
+      `}
+    >
+      <HFlow
+        style={css`
+          width: 100%;
+          height: 4rem;
+        `}
+      >
+        <Button
+          size="medium"
+          skin="ghost"
+          style={css`
+            color: white;
+          `}
+        >
+          SOBRE MIM
+        </Button>
+        <Button
+          size="medium"
+          skin="ghost"
+          style={css`
+            color: white;
+          `}
+        >
+          GITHUB
+        </Button>
+        <Button
+          size="medium"
+          skin="ghost"
+          style={css`
+            color: white;
+          `}
+        >
+          CONTATO
+        </Button>
+      </HFlow>
+    </VFlow>
+  );
+}
+
+export default Navbar;
